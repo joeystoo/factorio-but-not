07b2d1dca519973fc229bf24d33294994dcfb62c
joeystoo <easton.disimone@gmail.com>
2026-07-13 11:09:01 -0400

    entity dev

--- backEnd.py
+++ backEnd.py
@@ -1,5 +1,8 @@
+coordDict = {}
+
 class Entity:
-    def __init__(self, coords):
+    def __init__(self, coords, coordDict):
         self.coords = coords
         self.width = 2
         self.height = 2
+        coordDict[coords] = self
